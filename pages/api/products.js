f08fa349d06892a0393dff3b59432ba36af1b079
Alexandorsky <alvordar@gmail.com>
2020-05-09 16:03:31 -0600

    Adding initial mongo funcionality

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,13 @@
-import products from "../../static/products.json";
+//import products from "../../static/products.json";
+import Product from "../../models/Product";
+import connectDb from "../../utils/connectDb.js";
 
-export default (req, res) => {
-  console.log(req.method);
+connectDb();
 
+export default async (req, res) => {
+  const products = await Product.find();
   res.status(200).json(products);
+
+  //console.log(req.method);
+  //res.status(200).json(products);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import axios from "axios";
 
-function Home() {
-  React.useEffect(() => {
+function Home({ products }) {
+  console.log(products);
+
+  //React hook to run commands at this particular page lifecycle
+  /* React.useEffect(() => {
     getProducts();
   }, []);
 
-  function getProducts() {
+  async function getProducts() {
     const url = "http://localhost:3000/api/products";
-    axios.get(url);
+    const response = await axios.get(url);
+    console.log(response.data);
   }
-
+*/
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  //fetch data from server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  //return response data as an object
+  return { products: response.data };
+  //note: this object will be merged with existing props
+};
 
 export default Home;
